50176ec9aaee0b5cc1c8f875d79a51f826aea084
polka <polina.fesenko.04@gmail.com>
2026-04-17 17:28:47 +0300

    commit

---
+++ config/cors.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'paths'           => ['api/*'],
+    'allowed_methods'  => ['*'],
+
+    'allowed_origins'  => [
+        'http://localhost:5500',
+        'http://127.0.0.1:5500',
+        'null',  
+        ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers'  => ['*'],
+
+    'exposed_headers'  => [],
+    'max_age'          => 0,
+    'supports_credentials' => false,
+];
