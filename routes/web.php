8684502551054be7289a6f16776d191401e113d1
polka <polina.fesenko.04@gmail.com>
2026-04-18 03:43:44 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 
+Route::resource('books', BookController::class);
+Route::resource('authors', AuthorController::class);
 Route::get('/', function () {
     return view('welcome');
 });
